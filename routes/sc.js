b288194ef9294e8ac3a2be016bb2acc3dc5e2f32
Eduardo Aguilar <eaguilaortiz0403@gmail.com>
2024-06-11 17:30:55 -0600

    mini update

--- routes/sc.js
+++ routes/sc.js
@@ -5,7 +5,10 @@ const datosSC = (pool) => {
     
     console.log("datosSC");
     router.get("/infoService", async (req, res) => {
-        res.json('Hello, these are our current plans: \nPlan 1: $20 - 2 hours of cleaning \nPlan 2: $30 - 4 hours of cleaning \nPlan 3: 50$ - 5 hours of cleaning (Recommended for big houses)');
+        res.json('Hello, these are our current plans: \
+            Plan 1: $20 - 2 hours of cleaning \
+            Plan 2: $30 - 4 hours of cleaning \
+            Plan 3: 50$ - 5 hours of cleaning (Recommended for big houses)');
     });
     
 
